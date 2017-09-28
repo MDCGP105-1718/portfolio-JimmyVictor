f0a53e38cd6d6e161fa69b0238bc792bd6ff85dc
JimmyVictor <32356957+JimmyVictor@users.noreply.github.com>
2017-09-28 15:29:36 +0100

    Completed successfully

---
+++ ex4.py
@@ -0,0 +1,16 @@
+my_name = 'Sicoe Victor'
+my_age = 19
+my_height = 181 # centimeters
+my_weight = 78 # kilograms
+my_eyes = 'Brown'
+my_hair = 'Chestnut'
+is_heavy = my_weight > 9000
+
+print(f"Let's talk about {my_name}.")
+print(f"He is {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy.")
+print(f"It is {is heavy} that he is overweight.")
+print(f"He's got {my_eyes}) eyes and {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, {my_weight} I get {total}")
