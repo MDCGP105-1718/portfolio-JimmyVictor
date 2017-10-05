bb057df534815b817569c1aadcb6f444004e25d4
= <=>
2017-10-05 11:21:09 +0100

    You can write your details here -- for research purposes

---
+++ ex6.py
@@ -0,0 +1,19 @@
+my_name = input('Enter your name: ')
+my_gender = input('Enter your gender (male or female): ')
+my_age = int(input('Enter your age: '))
+if my_age<=14:
+    print('You are too young for this.')
+if my_age>=60:
+    print('You are too old for this.')
+my_height = int(input('Enter your height in centimeters: '))
+if my_height<=160:
+    print('Grow up!')
+if my_height>=200:
+    print('How is the weather up there?')
+my_weight = int(input('Enter your weight in kilograms: '))
+if my_weight<=45:
+    print('Give this person a sandwich!')
+if my_weight>=100:
+    print('Steal the food from this person!')
+my_eyes = input('Enter the color of your eyes: ')
+my_hair = input ('Enter the color of your hair: ')
