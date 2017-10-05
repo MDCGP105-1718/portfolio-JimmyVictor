283fc32e36a28f4c7e2849ea5962e31a146231a8
= <=>
2017-10-05 10:36:36 +0100

    there are still 1 BOTTLES

---
+++ ex5.py
@@ -0,0 +1,5 @@
+for n in range(99,0,-1):
+    if n>1:
+        print(n, "bottles of beer on the wall",n,"bottles of beer. Take one down, pass it around",n-1,"bottles of beer on the wall.")
+    else:
+        print (n, "bottle of beer on the wall",n,"bottles of beer. Take one down, pass it around",n-1,"no more bottles of beer on the wall, go to the store and buy some more, 99 bottles of beer.")
