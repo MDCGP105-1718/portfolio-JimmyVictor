2d66dbcdd8a7e2bef18549c5a37376a666007380
ccrewe <s171298@ucs.ac.uk>
2017-10-12 12:17:14 +0100

    I need to keep working on this

---
+++ ex9.py
@@ -0,0 +1,19 @@
+annual_salary = float(input('What is your annual salary?: '))
+
+semi_annual_raise = 0.7
+r = 0.04
+total_cost = 1000000
+down_payment = total_cost * 0.25
+monthly_salary = annual_salary / 12
+number_of_months = 36
+epsilon = 100
+num_guesses = 0
+low = 0
+high = 10000
+guess = (high + low) / 2.0
+while abs(guess - (current_savings - down_payment)) >= epsilon
+    if guess <
+
+
+
+    print('Steps in bisection search: ',num_guesses)
